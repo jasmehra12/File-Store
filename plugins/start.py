546f52dc31050d16fd6fa99dc8fc5e9de913b3f2
DARKY-LAB <nk1010057@gmail.com>
2024-12-27 16:33:21 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -82,8 +82,7 @@ async def start_command(client: Client, message: Message):
        # k = await client.send_message(chat_id=message.from_user.id, 
                                      # text=f"<b>⚠️ Wᴀʀɴɪɴɢ ⚠️\n\nTʜᴇsᴇ Fɪʟᴇ Wɪʟʟ Bᴇ Dᴇʟᴇᴛᴇᴅ Aᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ Iɴ 10Mɪɴ. Fᴏʀᴡᴀʀᴅ Tʜᴇsᴇ Mᴇssᴀɢᴇs...!</b>")
 
-        # Schedule the file deletion
-        #asyncio.create_task(delete_files(genanime_msg, client, k))
+         asyncio.create_task(delete_files(genanime_msg, client, k))
 
         return
     else:
