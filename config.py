7bd82d23d898e837860ca59b7b94fcac4a512314
jasmehra12 <jaskaran.1230j@gmail.com>
2025-04-01 08:47:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,29 +5,29 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6972029110:AAE7p6rF7fCwXjruQCn_dXXB59HOGlgzBNU")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7776271794:AAF90fUE1-Bd9dNv4JDdlNpk8B6i8XCJYFc")
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", "17417255"))
+APP_ID = int(os.environ.get("APP_ID", "22225430"))
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "73d424d9847f968130cd5b41946f7a5d")
+API_HASH = os.environ.get("API_HASH", "4c5c28abd62233ef4b993fb972f83262")
 #Your db channel Id
-CHANNEL_ID = os.environ.get("CHANNEL_ID", "@BatchBotLog")
+CHANNEL_ID = os.environ.get("CHANNEL_ID", "-1002311297760")
 # NAMA OWNER
-OWNER = os.environ.get("OWNER", "DARKXSIDE78")
+OWNER = os.environ.get("OWNER", "Jas_Mehra")
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "7086472788"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "6039119180"))
 #Port
 PORT = os.environ.get("PORT", "8030")
 #Database
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://nitinkumardhundhara:DARKXSIDE78@cluster0.wdive.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://botxhub69:Jasmehra1322@cluster0.a8eqa.mongodb.net/")
 DB_NAME = os.environ.get("DATABASE_NAME", "unknown")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = os.environ.get("FORCE_SUB_CHANNEL", "@rezero_dual_audio_01")
-FORCE_SUB_CHANNEL2 = os.environ.get("FORCE_SUB_CHANNEL2", "@GenAnimeOfc")
-FORCE_SUB_CHANNEL3 = os.environ.get("FORCE_SUB_CHANNEL3", "@HeroOrganizationManga")
+FORCE_SUB_CHANNEL = os.environ.get("FORCE_SUB_CHANNEL", "@hentaicomplex1")
+FORCE_SUB_CHANNEL2 = os.environ.get("FORCE_SUB_CHANNEL2", "@Jav_Complex")
+FORCE_SUB_CHANNEL3 = os.environ.get("FORCE_SUB_CHANNEL3", "@Pornhwa_Complex")
 
-TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "1"))
 
 FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "7200")) # auto delete in seconds
 
@@ -36,7 +36,7 @@ FORCE_PIC = os.environ.get("FORCE_PIC", "https://wallpapers.com/images/hd/naruto
 
 START_MSG = os.environ.get("START_MESSAGE", "<b>ʙᴀᴋᴋᴀᴀᴀ!!! <b>{first}</b>\n\n ɪ ᴀᴍ ғɪʟᴇ sᴛᴏʀᴇ ʙᴏᴛ, ɪ ᴄᴀɴ sᴛᴏʀᴇ ᴘʀɪᴠᴀᴛᴇ ғɪʟᴇs ɪɴ sᴘᴇᴄɪғɪᴇᴅ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴏᴛʜᴇʀ ᴜsᴇʀs ᴄᴀɴ ᴀᴄᴄᴇss ɪᴛ ғʀᴏᴍ sᴘᴇᴄɪᴀʟ ʟɪɴᴋ.</b>")
 try:
-    ADMINS=[6376328008]
+    ADMINS=[6039119180]
     for x in (os.environ.get("ADMINS", "6302971969 2077116559 1785065025 6844586745").split()):
         ADMINS.append(int(x))
 except ValueError:
@@ -58,7 +58,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "<b>ʙᴀᴋᴋᴀᴀᴀ! ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴍʏ ꜱᴇɴᴘᴀɪ!!!</b>"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(7086472788)
+ADMINS.append (6039119180)
 
 LOG_FILE_NAME = "bot.txt"
 
